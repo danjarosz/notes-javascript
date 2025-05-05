e1a3e697ec6d188fba70d6d081ffc3a77a477463
Daniel Jarosz <jarosz.daniel@protonmail.com>
2025-05-05 11:02:01 +0200

    strings

---
+++ values-types-operators/strings.js
@@ -0,0 +1,22 @@
+// Enclosing quotes
+
+const text1 = `Down on the sea`
+const text2 = "Lie on the ocean"
+const text3 = 'Float on the ocean'
+
+console.log({text1, text2, text3})
+
+// Multiple lines
+
+console.log("This is the first line\nAnd this is the second")
+console.log("A newline character is written like \"\\n\"")
+
+// Concatenation
+
+console.log(text1 + text3)
+console.log(text1 + text2)
+console.log(text2 + text3)
+
+// Template sting
+
+console.log(`Template string: ${text1}`)
