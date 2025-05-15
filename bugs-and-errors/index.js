a48914b2a9d721c8002feed50c5f52ab9bae1938
Daniel Jarosz <jarosz.daniel@protonmail.com>
2025-05-15 09:29:24 +0200

    errors and bugs

--- bugs-and-errors/index.js
+++ bugs-and-errors/index.js
@@ -1,3 +1,5 @@
+// Good description here https://eloquentjavascript.net/08_error.html
+
 function canYouSpotTheProblem() {
     "use strict";
     for (let counter = 0; counter < 10; counter++) {
@@ -18,3 +20,29 @@ function Person(name) {
 let ferdinand = new Person("Ferdinand"); // oops
 console.log(ferdinand.name);
 // → Ferdinand
+
+//-----------
+// The pattern to recognise error source
+
+class InputError extends Error {}
+
+function promptDirection(question) {
+    let result = prompt(question);
+    if (result.toLowerCase() == "left") return "L";
+    if (result.toLowerCase() == "right") return "R";
+    throw new InputError("Invalid direction: " + result);
+}
+
+for (;;) {
+    try {
+        let dir = promptDirection("Where?");
+        console.log("You chose ", dir);
+        break;
+    } catch (e) {
+        if (e instanceof InputError) {
+            console.log("Not a valid direction. Try again.");
+        } else {
+            throw e;
+        }
+    }
+}
